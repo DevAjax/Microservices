eca5e55414779e92a7961c3dd779e4df0a835c87
DevAjax <32197112+DevAjax@users.noreply.github.com>
2017-12-04 02:22:43 +0100

    -fixed tomcat bug

--- usersapi/src/main/java/com/backend/usersapi/controllers/UserSetController.java
+++ usersapi/src/main/java/com/backend/usersapi/controllers/UserSetController.java
@@ -1,13 +1,17 @@
 package com.backend.usersapi.controllers;
 
+import com.backend.usersapi.UsersApiApplication;
 import com.backend.usersapi.models.UserSet;
 import com.backend.usersapi.repositories.UserSetRepository;
+import com.google.gson.Gson;
 import io.jsonwebtoken.Claims;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedList;
@@ -15,24 +19,27 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/userSet")
+@RequestMapping("/userset")
 public class UserSetController {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(UsersApiApplication.class);
+
     @Autowired
     UserSetRepository userSetRepository;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public List<String> getUsersIdentifiers() {
+    public String getUsersIdentifiers() {
         List<String> res = new LinkedList<>();
         List<UserSet> set = userSetRepository.findAll();
         for (int i = 0; i < set.size(); i++) {
             res.add(set.get(i).getUserIdentifier());
         }
-        return res;
+        return new Gson().toJson(res);
     }
 
     @RequestMapping(value = "/{userIdentifier}", method = RequestMethod.GET)
     public UserSet getUserSet(HttpServletRequest req, @PathVariable("userIdentifier") String userIdentifier) {
         Object attributes = req.getAttribute("claims");
+        //  log.info(attributes.toString());
         if (!(attributes instanceof Claims) || (attributes == null)) {
             throw new RuntimeException("Invalid JSON Web Token");
         }
